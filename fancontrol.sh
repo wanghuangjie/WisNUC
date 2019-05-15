5f4de0f8830ae0c60fc2b067959488b3edd1e4ee
wanghuangjie <wanghuangjie@vip.qq.com>
2019-05-16 00:43:11 +0800

    Update fancontrol.sh

--- fancontrol.sh
+++ fancontrol.sh
@@ -4,8 +4,8 @@
 # v0.02
 cpu_temp=$(sensors -u|sed -n '/Core 0/ {n;p}'|awk {'print $2'}|sed 's/.000//') #获取cpu当前温度
 
-low=40 #低负载
-mid=50 #中负载
+low=35 #低负载
+mid=40 #中负载
 high=60  #高负载
 #--------------end of configure file--------------
 
@@ -17,11 +17,11 @@ if [ $cpu_temp -gt $high ]
 then
 change_fan_level 100 #全速
 echo PWR_LED 3 > /proc/BOARD_io #快闪
-elif [ $cpu_temp -gt $mid ] 
+elif [ $cpu_temp -gt $low ] 
 then
 change_fan_level 40 #中速
-elif [ $cpu_temp -gt $low ]
 echo PWR_LED 2 > /proc/BOARD_io #慢闪
+elif [ $cpu_temp -le $low ]
 then
 change_fan_level 25 #低速
 echo PWR_LED 0 > /proc/BOARD_io #常闭
