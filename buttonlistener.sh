c5567cfe946a05310220eca08060d03835f361e2
wanghuangjie <wanghuangjie@vip.qq.com>
2019-05-27 07:42:51 +0800

    按钮事件监听

--- buttonlistener.sh
+++ buttonlistener.sh
@@ -3,8 +3,8 @@ while true
 
 do
   button=$(< /proc/BOARD_event)
-#  echo $my_var
   if [ "$button" == "PWR ON" ]; then
+    # 相关按钮还包括 copy on、rst2df on、nobtn on
     echo "power pushed"
     curl "http://localhost:3000/api/v1/commands/?cmd=toggle"
   fi
